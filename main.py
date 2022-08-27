33fabda92c0345876ca5ea5317af47f4a8e12b04
Eobsv <d.eobsv@gamail.com>
2022-08-27 12:43:16 +0200

    Added functionality, and information when to change questions

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_q = Question(question_text, question_answer)
     question_bank.append(new_q)
 
 
 quiz = QuizBrain(question_bank)
 quiz.next_question()
+
+while quiz.still_has_question:
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your finals score is: {quiz.score}/{quiz.question_number}")
+
+#https://opentdb.com/api_config.php there is possibility to try many different quizes
